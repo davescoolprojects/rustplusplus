c6483abd522ed2f1bdb9c2ac5df1561ef40cbcc0
davescoolprojects <davidandmarelia@gmail.com>
2025-05-16 16:13:32 +1000

    Update index.js - ai command

--- config/index.js
+++ config/index.js
@@ -24,6 +24,7 @@ module.exports = {
         pollingIntervalMs: process.env.RPP_POLLING_INTERVAL || 10000,
         showCallStackError: process.env.RPP_LOG_CALL_STACK || false,
         reconnectIntervalMs: process.env.RPP_RECONNECT_INTERVAL || 15000,
+        aiApiKey: process.env.RPP_AI_API_KEY || ''
     },
     discord: {
         username: process.env.RPP_DISCORD_USERNAME || 'rustplusplus',
